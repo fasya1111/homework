6b181a3a3d6580dcc0903e26f738030370aea1f2
fasya1111 <112747433+fasya1111@users.noreply.github.com>
2022-09-03 10:41:58 +0400

    Add files via upload

---
+++ gthb.py
@@ -0,0 +1,17 @@
+
+from collections import deque
+
+que = deque()
+
+que.append('Apple')
+que.append('Mango')
+que.append('Banana')
+
+print(que)
+deque(['Apple ', 'Mango', 'Banana'])
+
+print(que.popleft())
+
+print(que.popleft())
+
+print(que.popleft())
